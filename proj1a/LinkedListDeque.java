c10e7b6eba2ab8f9ec5b45183071a9a0dabd8e35
Charon <74341017+44sunsetsf@users.noreply.github.com>
2022-01-21 13:01:48 +0800

    1a to store

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,72 @@
+public class LinkedListDeque <T> {
+    private class Node {
+        private Node prev;
+        private T item;
+        private Node next;
+
+        public Node(Node prev, T item, Node next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+    int size;
+    Node sentinel;
+
+    public LinkedListDeque() {
+       this.size = 0;
+       this.sentinel = new Node(null,(T) new Object(),null);
+       sentinel.prev = sentinel;
+       sentinel.next = sentinel;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public int Size(){
+        return size;
+    }
+    public void addFirst(T x){
+        Node newNode = new Node(sentinel,x,sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+    public void addLast (T x){
+        Node newNode = new Node(sentinel.prev,x,sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
+    }
+    public void printDeque(){
+        Node ptr = sentinel.next;
+        while (ptr != sentinel){
+            System.out.println(ptr.item + " ");
+            ptr = ptr.next;
+        }
+        }
+    public T removeFirst(){
+        if(isEmpty()) return null;
+        size --;
+        T res = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        return res;
+    }
+    public T removeLast(){
+        if(isEmpty()) return null;
+        size--;
+        T res = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        return res;
+    }
+    public T get(int index){
+        if(size <= index) return null;
+        Node ptr = sentinel.next;
+        for(int i = 0;i <= index;i++){
+            ptr = ptr.next;
+        }
+        return ptr.item;
+    }
+
+    }
