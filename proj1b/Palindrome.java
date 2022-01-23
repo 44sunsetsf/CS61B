5f3f8d729160d491fbd032e63114156792178914
Charon <74341017+44sunsetsf@users.noreply.github.com>
2022-01-23 21:02:28 +0800

    ok though half

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,20 @@
+import java.security.spec.PSSParameterSpec;
+
 public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
-        return null;
+        Deque<Character> result = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+           result.addLast(word.charAt(i));
+        }
+        return result;
+    }
+    public boolean isPalindrome(String word) {
+        Deque<Character> d = wordToDeque(word);
+        while (d.size() > 1) {
+            if (d.removeFirst() != d.removeLast()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
