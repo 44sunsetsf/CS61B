09a5879b41fa1aeb87d68963781fbb0c9be011d4
Charon <74341017+44sunsetsf@users.noreply.github.com>
2022-07-17 22:22:03 +0800

    ok HW2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,74 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
 public class Percolation {
+    private boolean[][] arr;
+    private int[][] arr1;
+    private WeightedQuickUnionUF qu;
+    private int count = 0;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        arr = new boolean[N][N];
+        arr1 = new int[N][N];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                arr[i][j] = false;
+                arr1[i][j] = helper(i, j);
+            }
+        }
+        qu = new WeightedQuickUnionUF(N * N + 2);
+        for (int j = 0; j < N; j++){
+            qu.union(arr1[0][j], N * N + 1);
+            qu.union(arr1[N - 1][j], N * N);
+        }
+
+    }
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col){
+        if (!isOpen(row, col)){
+            arr[row][col] = true;
+            count++;
+        if (isOpen(row - 1, col)){
+            qu.union(helper(row - 1, col), arr1[row][col]);
+        }
+        if (isOpen(row + 1, col)){
+            qu.union(helper(row + 1, col), arr1[row][col]);
+        }
+        if (isOpen(row, col + 1)){
+            qu.union(helper(row, col + 1), arr1[row][col]);
+        }
+        if (isOpen(row, col - 1)){
+            qu.union(helper(row, col - 1), arr1[row][col]);
+        }
+        }
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || col < 0 || row >= arr.length || col >= arr.length) {
+            return false;
+        }
+        return arr[row][col];
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (!isOpen(row, col))
+            return false;
+        return qu.connected(arr1[row][col],
+                arr.length * arr.length + 1);
+    }
+    // number of open sites
+    public int numberOfOpenSites() {
+        return count;
+    }
+    // does the system percolate?
+    public boolean percolates() {
+        return qu.connected(arr.length * arr.length + 1,
+                arr.length * arr.length);
+    }
+    // use for unit testing (not required)
+    private int helper(int row, int col) {
+        return (row * arr.length) + col;
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,63 @@
 package hw2;
 
-public class PercolationStats {
+import java.util.Random;
 
-}
+public class PercolationStats {
+    private Percolation[] p;
+    private Random RANDOM = new Random(565);
+    private static double[] x;
+    private double sum = 0;
+
+        // perform T independent experiments on an N-by-N grid
+        public PercolationStats(int N, int T, PercolationFactory pf) {
+            if (N <= 0 || T <= 0) {
+                throw new IllegalArgumentException(
+                        "java.lang.IllegalArgumentException");
+            }
+            p = new Percolation[T];
+            x = new double[T];
+            for (int i = 0; i < T; i++){
+                p[i] = pf.make(N);
+                while (!p[i].percolates()) {
+                    int a = RANDOM.nextInt(N);
+                    int b = RANDOM.nextInt(N);
+                    if (!p[i].isOpen(a,b)) {
+                        p[i].open(a, b);
+                    }else {
+                        continue;
+                    }
+                }
+                x[i] = ((double)(p[i].numberOfOpenSites())) / (N * N);
+                sum = sum + x[i];
+            }
+        }
+        // sample mean of percolation threshold
+        public double mean() {
+            return sum / x.length;
+        }
+        // sample standard deviation of percolation threshold
+        public double stddev() {
+            double u = mean();
+            double sum2 = 0;
+            for (int i = 0; i < x.length; i++) {
+                sum2 += (x[i] - u) * (x[i] - u);
+            }
+            double s = sum2 / (x.length - 1);
+            return Math.pow(s, 0.5);
+        }
+        // low endpoint of 95% confidence interval
+        public double confidenceLow() {
+            double u = mean();
+            double tmep = 1.96 * stddev() / (Math.pow(x.length,0.5));
+            return u - tmep;
+        }
+        // high endpoint of 95% confidence interval
+        public double confidenceHigh() {
+            double u = mean();
+            double tmep = 1.96 * stddev() / (Math.pow(x.length,0.5));
+            return u + tmep;
+        }
+
+    }
+
+
